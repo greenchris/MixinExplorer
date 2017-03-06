d114e54d82d83c07b429750b9ed989d583d2152f
Chris Green <Chris Green>
2017-03-07 00:32:23 +1300

    WIP - recovering custom json converter read ability.

--- src/MixinExplorer/Utilities/ToStringJsonConverter.cs
+++ src/MixinExplorer/Utilities/ToStringJsonConverter.cs
@@ -1,25 +1,54 @@
 ﻿using Newtonsoft.Json;
 using System;
+using MixinExplorer.Model;
+using Newtonsoft.Json.Linq;
 
 namespace MixinExplorer.Utilities
 {
-    public class ToStringJsonConverter : JsonConverter 
+    public class ToStringJsonConverter : JsonConverter
     {
-        public override bool CanConvert(Type objectType)
-        {
-            return true;
-        }
+        public override bool CanConvert(Type objectType) => true;
+        public override bool CanRead => true;
+
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-        {
-            writer.WriteValue(value.ToString());
-        }
-        public override bool CanRead
-        {
-            get { return false; }
-        }
+            => writer.WriteValue(value.ToString());
+
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
-            throw new NotImplementedException();
+            if (reader.TokenType == JsonToken.Null)
+                return null;
+
+            var jObject = JObject.Load(reader);
+            var target = new Ninja();
+
+            using (JsonReader jObjectReader = CopyReaderForObject(reader, jObject))
+            {
+                serializer.Populate(jObjectReader, target);
+            }
+            return target;
         }
+
+        #region private utils
+
+        /// <summary>
+        /// Creates new reader for the specified jObject, by copying the settings
+        /// from an existing reader.
+        /// </summary>
+        private static JsonReader CopyReaderForObject(JsonReader reader, JObject jObject)
+        {
+            JsonReader jObjectReader = jObject.CreateReader();
+
+            jObjectReader.Culture = reader.Culture;
+            jObjectReader.DateFormatString = reader.DateFormatString;
+            jObjectReader.DateParseHandling = reader.DateParseHandling;
+            jObjectReader.DateTimeZoneHandling = reader.DateTimeZoneHandling;
+            jObjectReader.FloatParseHandling = reader.FloatParseHandling;
+            jObjectReader.MaxDepth = reader.MaxDepth;
+            jObjectReader.SupportMultipleContent = reader.SupportMultipleContent;
+
+            return jObjectReader;
+        }
+
+        #endregion
     }
 }
